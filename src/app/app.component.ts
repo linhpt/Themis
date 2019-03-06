c832f232a4c5a00268baa4b0e0702df92dc9e5c6
Pham Tuan Linh <linh.pham2@ntq-solution.com.vn>
2019-03-06 19:34:48 +0700

    watch folder

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+var chokidar = (<any>window).require('chokidar');
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent  implements OnInit{
+  
   title = 'Themis Editor';
+
+  ngOnInit(): void {
+    var watcher = chokidar.watch(`C:\\Users\\linhp\\Documents\\personal\\testFolder`, {
+      ignored: /(^|[\/\\])\../,
+      persistent: true
+    });
+    
+    // Something to use when events are received.
+    var log = console.log.bind(console);
+    // Add event listeners.
+    watcher
+      .on('add', path => log(`File ${path} has been added`))
+      .on('change', path => log(`File ${path} has been changed`))
+      .on('unlink', path => log(`File ${path} has been removed`));
+    
+  }
 }
