882c4a8d3a5f0043dcb4032e4efa05d815c5137a
Pham Tuan Linh <linh.pham2@ntq-solution.com.vn>
2019-03-06 11:05:51 +0700

    upload and read excel

--- src/app/modules/room/room-details/room-details.component.ts
+++ src/app/modules/room/room-details/room-details.component.ts
@@ -4,6 +4,7 @@ import { ClassService } from 'src/app/core/services/class.service';
 import { IClass } from 'src/app/core/interfaces/core';
 import * as _ from 'lodash';
 import { UtilsService } from 'src/app/core/services/utils.service';
+import * as XLSX from 'xlsx';
 
 @Component({
   selector: 'app-room-details',
@@ -11,6 +12,9 @@ import { UtilsService } from 'src/app/core/services/utils.service';
   styleUrls: ['./room-details.component.css']
 })
 export class RoomDetailsComponent implements OnInit {
+
+  arrayBuffer: any;
+  file: File;
 
   constructor(
     private route: ActivatedRoute,
@@ -26,6 +30,26 @@ export class RoomDetailsComponent implements OnInit {
         const viewClass = _.find(classList, (cls: IClass) => cls.id == id);
       });
     });
+  }
+
+  incomingfile(event) {
+    this.file = event.target.files[0];
+  }
+
+  Upload() {
+    let fileReader = new FileReader();
+    fileReader.onload = (e) => {
+      this.arrayBuffer = fileReader.result;
+      var data = new Uint8Array(this.arrayBuffer);
+      var arr = new Array();
+      for (var i = 0; i != data.length; ++i) arr[i] = String.fromCharCode(data[i]);
+      var bstr = arr.join("");
+      var workbook = XLSX.read(bstr, { type: "binary" });
+      var first_sheet_name = workbook.SheetNames[0];
+      var worksheet = workbook.Sheets[first_sheet_name];
+      console.log(XLSX.utils.sheet_to_json(worksheet, { raw: true }));
+    }
+    fileReader.readAsArrayBuffer(this.file);
   }
 
 }
